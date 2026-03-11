da3e8d20a0160a56fa7d7073e4620437ab56400e
cloud-with-preetham <preetham.pereira@icloud.com>
2026-03-11 17:15:20 +0530

    updated return value index.html and port number 80

--- app.py
+++ app.py
@@ -1,13 +1,13 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 
 @app.route("/")
 def home():
-    return "DevSecOps Pipeline Running"
+    return render_template("index.html")
 
 
 if __name__ == "__main__":
     # Binding to all interfaces is required for container runtime.
-    app.run(host="0.0.0.0", port=5000)  # nosec B104
+    app.run(host="0.0.0.0", port=80)  # nosec B104
